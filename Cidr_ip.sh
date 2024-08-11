67ef775db4af9ca81a09bd7d0be605a8126eb4bd
DhanaLakshmi1779659 <81734112+DhanaLakshmi1779659@users.noreply.github.com>
2024-08-11 15:35:55 +0530

    Update Cidr_ip.sh

--- Cidr_ip.sh
+++ Cidr_ip.sh
@@ -1,35 +1,28 @@
+
 #!/bin/bash
 
-# Check if an argument is provided
-if [ "$#" -ne 1 ]; then
+# Check if a CIDR is provided
+if [ $# -ne 1 ]; then
     echo "Usage: $0 <CIDR>"
     exit 1
 fi
 
 CIDR=$1
 
-# Get network address and netmask
-NETWORK=$(ipcalc -n $CIDR | grep Network | awk '{print $2}')
-NETMASK=$(ipcalc -m $CIDR | grep Netmask | awk '{print $2}')
+# Extract the IP and prefix length from the CIDR
+IP=$(echo $CIDR | cut -d'/' -f1)
+PREFIX=$(echo $CIDR | cut -d'/' -f2)
 
-# Convert CIDR to IP address range
-IFS=. read -r i1 i2 i3 i4 <<< "$NETWORK"
-IFS=. read -r m1 m2 m3 m4 <<< "$NETMASK"
+# Convert the IP to its integer representation
+IFS=. read -r i1 i2 i3 i4 <<< "$IP"
+IP_DEC=$(( (i1 << 24) + (i2 << 16) + (i3 << 8) + i4 ))
 
-# Calculate the total number of IP addresses
-count=$(( ( (256 - m4) * 256 + (256 - m3) ) * 256 + (256 - m2) * 256 + (256 - m1) ))
+# Calculate the number of IPs in the range
+NUM_IPS=$(( 2 ** (32 - PREFIX) ))
 
-# Calculate the start and end IP addresses
-start_ip=$(( (i1 * 256 + i2) * 256 + i3 * 256 + i4 + 1 ))
-end_ip=$(( start_ip + count - 3 ))
-
-# Function to convert integer to IP address
-int_to_ip() {
-    local ip="$1"
-    echo "$((ip >> 24 & 255)).$((ip >> 16 & 255)).$((ip >> 8 & 255)).$((ip & 255))"
-}
-
-# List IP addresses
-for (( ip=start_ip; ip<=end_ip; ip++ )); do
-    int_to_ip $ip
+# Generate the IP addresses
+for ((i=0; i<NUM_IPS; i++)); do
+    IP_DEC_CURRENT=$(( IP_DEC + i ))
+    IP_CURRENT=$(printf "%d.%d.%d.%d\n" $(( (IP_DEC_CURRENT >> 24) & 255 )) $(( (IP_DEC_CURRENT >> 16) & 255 )) $(( (IP_DEC_CURRENT >> 8) & 255 )) $(( IP_DEC_CURRENT & 255 )))
+    echo $IP_CURRENT
 done
